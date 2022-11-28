2ed162657a6fece2766360fe2ffec594d7ce54c8
Djflights <djflights@gmail.com>
2022-11-28 15:17:43 -0700

    Finished Lab 7

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+'use strict'
 
-let car = "yes";
+//structure of conditional if/else statement
+// // if (this is true){execute code} else {execute code}
+// let car = "yes";
+// let buy = prompt("Are you looking to buy a Nissan 240?");
+// if (buy == car){
+//     alert("Thats awesome you should check your local Facebook Market place! Let me know what you find!");
+// } else {
+//     alert("That ok maybe next time!");
+// }
+// declared variable
+let usersName;
 
-let buy = prompt("Would you like to buy a Nissan 240?");
-if (buy == car){
-    alert("Thats awesome you should check your local Facebook Market place! Let me know what you find!");
-} else {
-    alert("That ok maybe next time!");
+function greetUser() {
+    usersName = prompt("What is your name?");
+    if (usersName == ""){
+        usersName = prompt ("Please enter your name!");
+    }
+    console.log(usersName);
+    document.write("Hi " + usersName + " welcome to my page!");
+    return usersName;
 }
-usersName = prompt("What is your name?");
-console.log("Hi " +usersName +" welcome to my page");
-document.write("Hi " + usersName + " welcome to my page!");
+greetUser();
+
+let buy;
+
+function askUser(user) {
+    
+    buy = prompt(user + " are you looking to buy a Nissan 240sx?");
+    buy =  buy.toLowerCase();
+    if (buy == "yes"){
+        alert("Thats awesome " + user + " you should check your local Facebook Market place! Let me know what you find!");
+    }
+     else {
+        alert("That ok maybe next time!");
+     }
+     return buy;
+
+    
+
+
+}
+
+askUser(usersName);
