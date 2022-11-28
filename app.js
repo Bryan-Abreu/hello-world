65ae7052a0f1c763a0134e2f54608277f6a43ee4
Djflights <djflights@gmail.com>
2022-11-27 18:41:00 -0700

    finished lab 6

---
+++ app.js
@@ -0,0 +1,12 @@
+
+let car = "yes";
+
+let buy = prompt("Would you like to buy a Nissan 240?");
+if (buy == car){
+    alert("Thats awesome you should check your local Facebook Market place! Let me know what you find!");
+} else {
+    alert("That ok maybe next time!");
+}
+usersName = prompt("What is your name?");
+console.log("Hi " +usersName +" welcome to my page");
+document.write("Hi " + usersName + " welcome to my page!");
